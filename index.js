832791ca925748f911305e8f5edd049e884d147b
RHsanto <rakibulsanto789@gmail.com>
2022-03-26 18:59:02 +0600

    add port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5200;
 
 //midalware
 app.use(cors());
